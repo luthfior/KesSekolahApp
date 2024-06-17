e01e1e98b8a81e0a6f485601d1225f9544913ccd
rafli <gimnastiarrafli@gmail.com>
2024-06-17 21:13:32 +0700

    + add search fragment

---
+++ app/src/main/java/com/example/kessekolah/ui/core/beranda/materi/searchMateri/SearchMateriFragment.kt
@@ -0,0 +1,158 @@
+package com.example.kessekolah.ui.core.beranda.materi.searchMateri
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kessekolah.R
+import com.example.kessekolah.data.database.MateriData
+import com.example.kessekolah.databinding.FragmentEditProfileBinding
+import com.example.kessekolah.databinding.FragmentSearchMateriBinding
+import com.example.kessekolah.model.ListMateriViewModel
+import com.example.kessekolah.ui.adapter.MateriListAdapter
+import com.example.kessekolah.ui.core.beranda.materi.listMateri.ListMateriFragmentDirections
+import com.example.kessekolah.viewModel.ViewModelFactoryBookMark
+
+
+class SearchMateriFragment : Fragment() {
+
+    private var _binding: FragmentSearchMateriBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: ListMateriViewModel
+    lateinit var materiList: ArrayList<MateriData>
+    private lateinit var adapter: MateriListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchMateriBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val vmFactory = ViewModelFactoryBookMark.getInstance(requireActivity().application)
+
+        viewModel = ViewModelProvider(
+            requireActivity(),
+            vmFactory
+        )[ListMateriViewModel::class.java]
+
+        setupData()
+
+    }
+
+    private fun setupData() = with(binding) {
+        adapter = MateriListAdapter()
+        rvMateri.adapter = adapter
+        rvMateri.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter.setOnItemClickCallback(object : MateriListAdapter.OnItemClickCallback {
+
+            override fun onDeleteClicked(data: MateriData) {
+//                showDialog(data)
+            }
+
+            override fun onEditClicked(data: MateriData) {
+                val action = SearchMateriFragmentDirections.actionSearchMateriFragmentToEditMateriFragment(data)
+                findNavController().navigate(action)
+            }
+
+            override fun onItemClicked(data: MateriData) {
+                val action = SearchMateriFragmentDirections.actionSearchMateriFragmentToDetailMateriFragment(data)
+                findNavController().navigate(action)
+            }
+
+        })
+
+        viewModel.materiList.observe(viewLifecycleOwner) { data ->
+            data?.let {
+                materiList = it as ArrayList<MateriData>
+                adapter.submitList(it)
+            }
+        }
+
+        textJudulMateri.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filter(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.search_menu, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
+        val searchView: SearchView = searchItem.getActionView() as SearchView
+
+        searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(msg: String): Boolean {
+                filter(msg)
+                return false
+            }
+        })
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.actionSearch -> return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+
+
+    private fun filter(text: String) {
+        // creating a new array list to filter our data.
+        val filteredlist: ArrayList<MateriData> = ArrayList()
+
+        // running a for loop to compare elements.
+        for (item in materiList) {
+            // checking if the entered string matched with any item of our recycler view.
+            if (item.judul.toLowerCase().contains(text.toLowerCase())) {
+                // if the item is matched we are
+                // adding it to our filtered list.
+                filteredlist.add(item)
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            // if no item is added in filtered list we are
+            // displaying a toast message as no data found.
+            Toast.makeText(requireContext(), "No Data Found..", Toast.LENGTH_SHORT).show()
+        } else {
+            // at last we are passing that filtered
+            // list to our adapter class.
+            adapter.filterList(filteredlist)
+        }
+    }
+
+}
